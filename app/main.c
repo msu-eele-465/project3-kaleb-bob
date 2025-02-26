679f671c67d4012fd3dddc1087a2c08c897cbc23
Kaleb Jones <littlebearkk@Kaleb-Jones-MBP.lan>
2025-02-26 09:39:15 -0700

    added RGB code

--- app/main.c
+++ app/main.c
@@ -2,75 +2,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-int main(void){
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-    
-    // Disable the GPIO power-on default high-impedance mdoe to activate
-    // previously configure port settings
-    PM5CTL0 &= ~LOCKLPM5;
-
-    // setup of GPIO Port 3
-    // cols : P3.0 - P3.3 on MSP, Pins 1 - 4 on Keypad
-    // rows : P3.4 - P3.7 on MSP, Pins 5 - 8 on Keypad
-
-    //   Value, MSP Col, MSP Row, KeyPad Pin Col, Keypad Pin Row
-
-    //    || 1, P3.3, P3.7, 4, 8 | 2, P3.2, P3.7, 3, 8 | 3, P3.1, P3.7, 2, 8 | A, P3.0, P3.7, 1, 8 ||
-    //    || 4, P3.3, P3.6, 4, 7 | 5, P3.2, P3.6, 3, 7 | 6, P3.1, P3.6, 2, 7 | B, P3.0, P3.6, 1, 7 ||
-    //    || 7, P3.3, P3.5, 4, 6 | 8, P3.2, P3.5, 3, 6 | 9, P3.1, P3.5, 2, 6 | C, P3.0, P3.5, 1, 6 ||
-    //    || *, P3.3, P3.4, 4, 5 | 0, P3.2, P3.4, 3, 5 | #, P3.1, P3.4, 2, 5 | D, P3.0, P3.4, 1, 5 || 
-
-
-   /* while(true)
-    {
-    } */
-
-    // set up P6.6 (LED) as output
-    P6DIR |= BIT6;
-    P6OUT &= ~BIT6;
-
-    // Set up P1.0 (LED) as output
-    P1DIR |= BIT0;
-    P1OUT &= ~BIT0;
-
-    int row[] = {0, 0, 0, 0};
-    int col[] = {0, 0, 0, 0};
-
-    int i;
-    i = 0;
-
-    row[i] = findRow();
-    col[i] = findCol();
-    i++;
-
-    while(i < 4)
-    {
-        if(P3IN == 240) // if button has been released
-        {
-            row[i] = findRow();
-            col[i] = findCol();
-            i++;
-        }
-        else {
-        }
-    }
-
-    if(row[0] == 8 && row[1] == 8 && row[2] == 7 && row[3] == 5)
-    {
-        if(col[0] == 1 && col[1] == 2 && col[2] == 3 && col[3] == 1)
-        {
-            P1OUT |= BIT0;
-            P6OUT |= BIT6;
-        }
-    }
-
-
-
-    return 0;
-
-}
-
 int findRow(void)
 {
    // set up columns as outputs
@@ -95,6 +26,7 @@ int findRow(void)
    P3OUT &= ~BIT6;
    P3OUT &= ~BIT7; 
 
+
    // set all columns HIGH
    P3OUT |= BIT3;
    P3OUT |= BIT2;
@@ -102,6 +34,7 @@ int findRow(void)
    P3OUT |= BIT0;
 
    int keyIn;
+   int row;
 
    // reading row input (00001111 - 15)
    // if button is pushed, one of these four
@@ -109,36 +42,29 @@ int findRow(void)
     // 01001111 - 79, ROW 7
     // 00101111 - 47, ROW 6
     // 00011111 - 31, ROW 5
-    int i;
-    int row;
     keyIn = P3IN;
 
     switch(keyIn)
     {
         case 143:           // in ROW 8
-            P1OUT &= ~BIT0;
-            P6OUT |= BIT6;
-            return 8;
+            row = 8;
+            break;              
         case 79:            // in ROW 7
-            P1OUT &= ~BIT0;
-            P6OUT |= BIT6;
-            return 7;
+            row = 7;
+            break;
         case 47:            // in ROW 6
-            P1OUT &= ~BIT0;
-            P6OUT |= BIT6;
-            return 6;
+            row = 6;
+            break;
         case 31:            // in ROW 5
-            P1OUT &= ~BIT0;
-            P6OUT |= BIT6;
-            return 5;
-        default:            // no buttons pushed
-            P1OUT |= BIT0;
-            P6OUT &=~ BIT6;
-            findRow();      // call function again until button is pushed
+            row = 5;
+            break;
+        case 15:
+            row = 10;     // out of range!!
 
     }
-    
+    return row;
 }
+
 
 int findCol(void)
 {
@@ -173,24 +99,357 @@ int findCol(void)
    P3OUT &= ~BIT2;
    P3OUT &= ~BIT3; 
 
-    int i;
     int col;
     int keyIn;
     keyIn = P3IN;
 
+    // reading col input (11110000 - 240)
+    // if button is pushed, one of these four
+    // 11110001 - 241, COL 1
+    // 11110010 - 242, COL 2
+    // 11110100 - 244, COL 3
+    // 11111000 - 248, COL 4
+
     switch(keyIn)
     {
         case 241:           // in COL 1
-            return 1;
+            col = 1;
+            break;
         case 242:            // in COL 2
-            return 2;
+            col = 2;
+            break;
         case 244:            // in COL 3
-            return 3;
+            col = 3;
+            break;
         case 248:            // in COL 4
-            return 4;
-        default:            // no buttons pushed
-            findCol();       // call function again
-
+            col = 4;
+            break;
+      //  default:            // no buttons pushed
+        //    findRow();
     }
 
+    return col;
+
 }
+
+bool keyReleased(void)
+{
+        // show GREEN until code is entered
+    int keyPush;
+    keyPush = P3IN;
+    if (keyPush < 241) // key released
+    {
+        return true;
+    }
+    else {              // key NOT released
+        return false;
+    }
+}
+
+
+int main(void){
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    
+    // Disable the GPIO power-on default high-impedance mdoe to activate
+    // previously configure port settings
+    PM5CTL0 &= ~LOCKLPM5;
+
+    // setup of GPIO Port 3
+    // cols : P3.0 - P3.3 on MSP, Pins 1 - 4 on Keypad
+    // rows : P3.4 - P3.7 on MSP, Pins 5 - 8 on Keypad
+
+    //   Value, MSP Col, MSP Row, KeyPad Pin Col, Keypad Pin Row
+
+    //    || 1, P3.3, P3.7, 4, 8 | 2, P3.2, P3.7, 3, 8 | 3, P3.1, P3.7, 2, 8 | A, P3.0, P3.7, 1, 8 ||
+    //    || 4, P3.3, P3.6, 4, 7 | 5, P3.2, P3.6, 3, 7 | 6, P3.1, P3.6, 2, 7 | B, P3.0, P3.6, 1, 7 ||
+    //    || 7, P3.3, P3.5, 4, 6 | 8, P3.2, P3.5, 3, 6 | 9, P3.1, P3.5, 2, 6 | C, P3.0, P3.5, 1, 6 ||
+    //    || *, P3.3, P3.4, 4, 5 | 0, P3.2, P3.4, 3, 5 | #, P3.1, P3.4, 2, 5 | D, P3.0, P3.4, 1, 5 || 
+
+
+   /* while(true)
+    {
+    } */
+
+
+    // set up P6.6 (LED) as output
+   // P6DIR |= BIT6;
+   // P6OUT &= ~BIT6;
+
+    // Set up P1.0 (LED) as output
+   // P1DIR |= BIT0;
+   // P1OUT &= ~BIT0;
+
+   unlock();
+}
+
+void unlock(void)
+{
+
+    RGB(0);
+
+    int r;
+    int c;
+    r = 10;
+
+    int i;
+    i = 0;
+
+    bool done;
+    done = false;
+    int code;
+    code = 0;
+
+    while(i < 4)
+    {
+        while(r == 10)
+        {
+            r = findRow();
+        }
+
+        c = findCol();
+
+        while(done != true)
+        {
+            done = keyReleased();
+        }
+        RGB(1);
+
+
+        switch(i)
+        {
+            case 0:
+                if (r == 8)
+                {
+                    if (c == 2)
+                    {
+                        code++;
+                    }
+                }
+            case 1:
+                if (r == 7)
+                {
+                    if (c == 3)
+                    {
+                        code++;
+                    }
+                }
+            case 2:
+                if (r == 5)
+                {
+                    if (c == 2)
+                    {
+                        code++;
+                    }
+                }
+            case 3:
+                if (r == 7)
+                {
+                    if (c == 1)
+                    {
+                        code++;
+                    }
+                }
+        }
+
+        done = false;
+        r = 10;
+        i++;
+    }
+
+
+    if (code == 4) // unlocked
+    {
+        RGB(2);
+        selectPattern();
+    }
+    else {          // locked
+        RGB(0);
+        unlock();
+    }
+}
+
+void selectPattern(void)
+{
+        bool done;
+        done = false;
+
+        int r;
+        int c;
+        r = 10;
+
+        int ledPatt;
+        int prevPatt;
+
+        while(true)
+        {
+            while(r == 10)
+            {
+                r = findRow();
+            }
+
+            c = findCol();
+
+            while(done != true)
+            {
+                done = keyReleased();
+            }
+
+            switch(r)
+            {
+                case 8:
+                    switch(c)
+                    {
+                        case 1:
+                        case 2:
+                            ledPatt = 3;
+                            break;
+                        case 3:
+                            ledPatt = 2;
+                            break;
+                        case 4:
+                            ledPatt = 1;
+                            break;
+                    }
+                    break;
+                case 7:
+                    switch(c)
+                    {
+                        case 1:
+                        case 2:
+                            ledPatt = 6;
+                            break;
+                        case 3:
+                            ledPatt = 5;
+                            break;
+                        case 4:
+                            ledPatt = 4;
+                            break;
+                    }
+                    break;
+                case 6:
+                    switch(c)
+                    {
+                        case 1:
+                        case 2:
+                            ledPatt = 9;
+                            break;
+                        case 3:
+                            ledPatt = 8;
+                            break;
+                        case 4:
+                            ledPatt = 7;
+                            break;
+                    }
+                    break;
+                case 5:
+                    switch(c)
+                    {
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                    }
+                    break;
+            }
+
+            setup();
+            loop(ledPatt);
+            done = false;
+            r = 10;
+
+        }
+}
+
+void RGB(int color)
+{
+    
+    // setup RGB LED pinout
+    P6DIR |= BIT0; // RED
+    P6DIR |= BIT1; // GREEN
+    P6DIR |= BIT2; // BLUE
+
+    switch(color)
+    {
+        case 0: // locked
+            P6OUT &= ~BIT0; // RED
+            P6OUT |= BIT1; // GREEN
+            P6OUT |= BIT2; // BLUE
+            break;
+        case 1: // being unlocked
+            P6OUT &= ~BIT0; // RED
+            P6OUT &= ~BIT1; // GREEN
+            P6OUT |= BIT2; // BLUE
+            break;
+        case 2: // unlocked
+            P6OUT &= ~BIT0; // RED
+            P6OUT |= BIT1; // GREEN
+            P6OUT &= ~BIT2; // BLUE
+            break;
+
+    }
+    return;
+
+}
+/*
+void RGBlock(void)
+{
+    TB0CTL |= TBCLR;    // clear timer and dividers
+    TB0CTL |= TBSSEL__ACLK; // Source = ACLK (32768k)
+    TB0CTL |= MC__UP;   // mode up
+
+    TB1CTL |= TBCLR;    // clear timer and dividers
+    TB1CTL |= TBSSEL__ACLK; // Source = ACLK (32768k)
+    TB1CTL |= MC__UP;   // mode up
+
+    TB0CCTL0 |= CCIE;   // enable TB0CCR0 Overflow IRQ
+    TB0CCTL0 &= ~CCIFG; // clear CCR0 flag
+    TB0CCTL1 |= CCIE;   // enable TB0CCR1 Overflow IRQ
+    TB0CCTL1 &= ~CCIFG; // clear CCR1 flag
+    TB0CCTL2 |= CCIE;   // enable TB0CCR2 Overflow IRQ
+    TB0CCTL2 &= ~CCIFG; // clear CCR1 flag
+    TB1CCTL0 |= CCIE;   // enable TB1CCR0 Overflow IRQ
+    TB1CCTL0 &= ~CCIFG; // clear CCR1 flag
+    __enable_interrupt();
+    
+
+
+
+    TB0CCR0 = 32768;    // CCR0 = 32768 // period of pulse
+    TB0CCR1 = 25165;    // R
+    TB0CCR2 = 7962;     // G
+
+    TB1CCR0 = 3725;     // B
+
+    while(1){}
+}
+
+#pragma vector = TIMER0_B0_VECTOR // puts everything high
+__interrupt void ISR_TB0_CCR0(void)
+{
+    P6OUT |= BIT0; // RED
+    P6OUT |= BIT1; // GREEN
+    P6OUT |= BIT2; // BLUE
+    TB0CCTL0 &= ~CCIFG;
+}
+
+#pragma vector = TIMER0_B1_VECTOR // pulls blue low
+__interrupt void ISR_TB0_CCR1(void)
+{
+    P6OUT &= ~BIT2; // blue
+    TB0CCTL1 &= ~CCIFG;
+}
+
+#pragma vector = TIMER0_B2_VECTOR // pulls green low
+__interrupt void ISR_TB0_CCR2(void)
+{
+    P6OUT &= ~BIT1; //green
+    TB0CCTL2 &= ~CCIFG;
+}
+
+#pragma vetor = TIMER1_B0_VECTOR // pulls red low
+__interrupt void ISR_TB1_CCR0(void)
+{
+    P6OUT &= ~BIT0; // red
+    TB1CCTL0 &= ~CCIFG;
+} */
